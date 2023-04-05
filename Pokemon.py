87cdd5bdd31d94c69cb51dde3c5157aaf42ac557
ThomasQTruong <TruongThomasQ@gmail.com>
2023-04-04 17:07:39 -0700

    Prepping to implement Pokemon.

---
+++ Pokemon.py
@@ -0,0 +1,13 @@
+"""
+ * Pokemon.py
+ * Holds the data of pokemon.
+ * 
+ * Copyright (c) 2023, Thomas Truong
+"""
+
+class Pokemon:
+  # General information.
+  name = ""
+  pokeIndex = 0
+  
+  # Spawn conditions.
