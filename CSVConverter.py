54a7ef2d4c91dd836f694cbcba8935ab5badeebe
Thomas Truong <TruongThomasQ@gmail.com>
2023-04-07 04:06:06 -0700

    Fully revamped code.

--- CSVConverter.py
+++ CSVConverter.py
@@ -1,122 +1,139 @@
 """
  * CSVConverter.py
- * Converts .json QALPS data files into CSV.
+ * Converts .json Cobblemon data files into CSV.
  * 
  * Copyright (c) 2023, Thomas Truong
 """
 
-import os, csv, json, Pokemon
+import os, csv, json, CobblemonData
+from typing import Union
 
 # Directory to read data files from.
-QALPS_DIR = "./QALPS/"
+DATA_DIR = "./CobblemonData/"
+
+# Contains all the headers and where to get the values.
+HEADERS = {
+  "No.": "getIndex",
+  "Pokemon": "spawns/pokemon",
+  "Biome": "conditions/biomes",
+  "Excluded": "anticonditions/biomes",
+  "Time": "conditions/timeRange",
+  "Weather": "getWeather",
+  "Context": "spawns/context",
+  "Preset": "spawns/presets",
+  "Requirements": "getRequirements",
+  "Bucket": "spawns/bucket",
+  "Weight": "spawns/weight",
+  "Lv. Min": "getMinLevel",
+  "Lv. Max": "getMaxLevel",
+  "canseeSky": "conditions/canSeeSky" 
+}
+
 
 def main():
   # Create CSV in write mode.
-  with open("cobblemonQALPS.csv", "w") as csvFile:
+  with open("CobblemonData.csv", "w") as csvFile:
     csvWriter = csv.writer(csvFile, quotechar='"', quoting=csv.QUOTE_MINIMAL)
     # Create headers.
-    csvWriter.writerow(["No.", "Pokemon", "Biome", "Excluded", "Time", "Weather", "Context", "Preset",
-                         "Requirements", "Bucket", "Weight", "Lv. Min", "Lv. Max", "canseeSky"])
+    csvWriter.writerow(HEADERS.keys())
 
     # Gets and sorts directory.
-    sortedDirectory = sorted(os.listdir(QALPS_DIR))
+    sortedDirectory = sorted(os.listdir(DATA_DIR))
 
     # For every fileName in directory.
     for fileName in sortedDirectory:
       # Obtain Pokemon Index.
       pIndex = str(int(fileName.split("_")[0]))
-      pFile = open(QALPS_DIR + fileName, "r")
+      pFile = open(DATA_DIR + fileName, "r")
       # Obtain json data.
       pData = json.load(pFile)
 
       for spawn in pData["spawns"]:
-        # Get pokemon name.
-        pName = cleanJsonValue(spawn["pokemon"]).capitalize()
-        
-        # Get and format biome(s).
-        pBiomes = cleanJsonArray(spawn["condition"]["biomes"])
-        
-        # Get and format exclusion if exists.
-        pExcluded = ""
-        if ("anticondition" in spawn):
-          pExcluded = cleanJsonArray(spawn["anticondition"]["biomes"])
+        # Create a temporary pokemon object that stores data.
+        tempData = CobblemonData.CobblemonData()
+        tempData.index = pIndex
 
-        # Get and format time if exists.
-        pTime = "any"
-        if ("timeRange" in spawn["condition"]):
-          pTime = cleanJsonValue(spawn["condition"]["timeRange"])
-        
-        # Get and format weather if exists.
-        pWeather = "any"
-        if ("isThundering" in spawn["condition"] and cleanJsonValue(spawn["condition"]["isThundering"]) == "true"):
-          pWeather = "storm"
-        elif ("isRaining" in spawn["condition"]):
-          if (cleanJsonValue(spawn["condition"]["isRaining"]) == "true"):
-            pWeather = "rain"
-          else:
-            pWeather = "clear"
+        # Extract all general information if possible.
+        for key in tempData.spawns.keys():
+          if (key in spawn):
+            tempData.setSpawn(key, cleanJsonValue(spawn[key]))
 
-        # Get context.
-        pContext = cleanJsonValue(spawn["context"])
+        # Extract all spawn conditions if possible.
+        for key in tempData.conditions.keys():
+          if (key in spawn["condition"]):
+            tempData.setCondition(key, cleanJsonValue(spawn["condition"][key]))
 
-        # Get preset(s) if exist.
-        pPresets = ""
-        if ("presets" in spawn):
-          pPresets = cleanJsonArray(spawn["presets"])
-
-        # Get and format requirement(s).
-        pMinY = ""
-        pMaxY = ""
-        pMinLight = ""
-        pReqBlocks = ""
-        if ("minY" in spawn["condition"]):  # Get minY if exists.
-          pMinY = cleanJsonValue(spawn["condition"]["minY"])
-          pMinY = "minY = " + pMinY
-        if ("maxY" in spawn["condition"]):  # Get maxY if exists.
-          pMaxY = cleanJsonValue(spawn["condition"]["maxY"])
-          pMaxY = "maxY = " + pMaxY
-        if ("minLight" in spawn["condition"]):  # Get minLight if exists.
-          pMinLight = cleanJsonValue(spawn["condition"]["minLight"])
-          pMinLight = "minLight = " + pMinLight
-        if ("neededNearbyBlocks" in spawn["condition"]): # Get required blocks if exists.
-          pReqBlocks = cleanJsonArray(spawn["condition"]["neededNearbyBlocks"])
-        pRequirement = ", ".join(filter(None, [pMinY, pMaxY, pMinLight, pReqBlocks]))
-
-        # Get bucket.
-        pBucket = cleanJsonValue(spawn["bucket"])
-        
-        # Get Weight.
-        pWeight = cleanJsonValue(spawn["weight"])
-        
-        # Get level range.
-        pLvRange = cleanJsonValue(spawn["level"]).split("-")
-        pLvMin = pLvRange[0]
-        if (len(pLvRange) == 2):
-          pLvMax = pLvRange[1]
-        else:
-          pLvMax = pLvMin
-
-        # Get seeSky attribute.
-        pSeeSky = ""
-        if ("canSeeSky" in spawn["condition"]):
-          pSeeSky = cleanJsonValue(spawn["condition"]["canSeeSky"])
+        # Extract all anti-conditions if possible.
+        for key in tempData.anticonditions.keys():
+          if ("anticondition" in spawn):
+            if (key in spawn["anticondition"]):
+              tempData.setAnticondition(key, cleanJsonValue(spawn["anticondition"][key]))
         
         # Write all the information into the row.
-        csvWriter.writerow([pIndex, pName, pBiomes, pExcluded, pTime, pWeather, pContext, pPresets,
-                             pRequirement, pBucket, pWeight, pLvMin, pLvMax, pSeeSky])
-      
+        csvWriter.writerow(getHeaderValues(tempData))
+        
       # Close file.
       pFile.close()
 
 
-# Cleans the .json values.
-def cleanJsonValue(jsonVal):
-  return json.dumps(jsonVal).replace('"', '')
+def cleanJsonValue(jsonVal: Union[str, float, list, bool, int]) -> Union[str, None]:
+  """Returns cleaned version of JSON value.
+
+  Removes brackets from JSON, extra quotations, and turns it into a string.
+
+  Args:
+    jsonVal:
+      The JSON value to clean up.
+  
+  Returns:
+    The clean JSON value or None if nothing given.
+  """
+
+  # Nothing given.
+  if (len(str(jsonVal)) == 0):
+    return
+
+  # Convert to JSON string.
+  clean = json.dumps(jsonVal)
+  # Is a list (has open bracket), remove brackets.
+  if (len(clean) >= 2 and clean[0] == "["):
+    clean = clean[1:-1]
+  # Get rid of "s.
+  clean = clean.replace('"', '')
+  
+  return clean
 
 
-# Cleans the .json arrays.
-def cleanJsonArray(jsonArr):
-  return json.dumps(jsonArr)[1:-1].replace('"', '')
+def getHeaderValues(pokeData: CobblemonData) -> list:
+  """Returns the values of each header.
+  
+  Retrieves the value of each header and puts it into a list to return.
+
+  Args:
+    pokeData:
+      The object that contains a Pokemon's data.
+
+  Returns:
+    The list of values of each header.
+  """
+  headerValues = []
+
+  # For every header.
+  for header in HEADERS:
+    valueLocation = HEADERS[header].split("/")
+    # Getter function has value.
+    if (len(valueLocation) == 1):
+      headerValues.append(getattr(pokeData, valueLocation[0])())
+    # Dictionary has value.
+    else:
+      headerValues.append(getattr(pokeData,
+                                  valueLocation[0])[valueLocation[1]])
+      
+  # Capitalize all the Pokemon names.
+  headerValues[1] = headerValues[1].title()
+
+  return headerValues
+
 
 
 if __name__ == "__main__":
